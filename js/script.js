5f7fd09cff4278b46abb2d5d25df9c2ec4eeab35
vishalkrsharma <vishalkrsharma1234@gmail.com>
2021-08-03 02:09:28 +0530

    event-div added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,42 @@ const date = new Date();
 
 let count = 0;
 
+const renderEvent = () => {
+    const openEventBtn = document.querySelectorAll('[data-event-target]');
+    const closeEventBtn = document.querySelectorAll('[data-close-button]');
+    const overlay = document.getElementById('overlay');
+    
+    openEventBtn.forEach(button => {
+        button.addEventListener('click', () => {
+            const event = document.querySelector(button.dataset.eventTarget);
+            openEvent(event);
+        });
+    });
+    
+    closeEventBtn.forEach(button => {
+        button.addEventListener('click', () => {
+            const event = button.closest('.event');
+            closeEvent(event);
+        });
+    });
+    
+    function openEvent (event) {
+        if(event == null) {
+            return;
+        }
+        event.classList.add('active');
+        overlay.classList.add('active');
+    }
+    
+    function closeEvent (event) {
+        if(event == null) {
+            return;
+        }
+        event.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+};
+
 const renderCalendar = () => {
     date.setDate(1);
 
@@ -26,19 +62,19 @@ const renderCalendar = () => {
     let days = "";
 
     for(let x=firstDayIndex; x>0; x--){
-        days += `<div class="prev-date">${prevLastDay - x +1}</div>`;
+        days += `<button class="prev-date">${prevLastDay - x +1}</button>`;
     }
 
     for(let i=1;i<=lastDay;i++) {
         if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
-            days += `<div class="today">${i}</div>`;
+            days += `<button class="today" data-event-target="#event">${i}</button>`;
         } else {
-            days += `<div>${i}</div>`;
+            days += `<button data-event-target="#event">${i}</button>`;
         }
     }
 
     for(let j=1; j<=nextDays; j++) {
-        days+=`<div class="next-date">${j}</div>`;
+        days+=`<button class="next-date">${j}</button>`;
         monthDays.innerHTML = days;
     }
 };
@@ -47,12 +83,14 @@ document.querySelector('.prev').addEventListener('click', () => {
     date.setMonth(date.getMonth() - 1);
     count--;
     renderCalendar();
+    renderEvent();
 });
 
 document.querySelector('.next').addEventListener('click', () => {
     date.setMonth(date.getMonth() + 1);
     count++;
     renderCalendar();
+    renderEvent();
 });
 
 
@@ -60,6 +98,14 @@ document.querySelector('button').addEventListener('click', () => {
     date.setMonth(date.getMonth() - count);
     count=0;
     renderCalendar();
+    renderEvent();
 });
 
 renderCalendar();
+
+renderEvent();
+
+
+
+
+
